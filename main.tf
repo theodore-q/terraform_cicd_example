837fd7ba0150eb35d2483485215bee41eceb2011
theodore-q <theo.q.er@gmail.com>
2023-06-17 12:24:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 data "template_file" "user_data" {
   template = file("${path.module}/user_data.sh")
+
+  vars = {
+    server_js    = file("${path.module}/server.js")
+    package_json = file("${path.module}/package.json")
+  }
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-00b1c9efd33fda707" # update with your preferred AMI ID
+  ami           = "ami-0c94855ba95c574c8" # update with your preferred AMI ID
   instance_type = "t2.micro"
   user_data     = data.template_file.user_data.rendered
 
