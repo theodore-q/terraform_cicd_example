677bdd2338be5792dafe28e7d79f373778212444
unknown <theo.q.er@gmail.com>
2023-06-17 12:00:49 +0100

    foo

--- user_data.sh
+++ user_data.sh
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
+  console.log(`Server running at http://localhost:$${port}`)
 })" > server.js
 
 # Start the app
