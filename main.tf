b130fc1d72149fc4d6393a032f17e206a464f036
theodore-q <theo.q.er@gmail.com>
2023-06-17 12:43:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,37 @@ data "template_file" "user_data" {
   }
 }
 
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-00b1c9efd33fda707" # update with your preferred AMI ID
   instance_type = "t2.micro"
   user_data     = data.template_file.user_data.rendered
+  vpc_security_group_ids = [aws_security_group.example.id]
 
   tags = {
     Name = "terraform-example"
