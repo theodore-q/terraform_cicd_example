182e073ef25a569d7593e491d21edfd15fe0a6e7
unknown <theo.q.er@gmail.com>
2023-06-17 13:14:57 +0100

    update ssh

--- user_data.sh
+++ user_data.sh
@@ -3,7 +3,12 @@
 # Update and install necessary packages
 yum update -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-. ~/.nvm/nvm.sh
+
+# Export NVM_DIR to make sure we're using the correct one
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Install node using nvm
 nvm install node
 
 # Create app directory
