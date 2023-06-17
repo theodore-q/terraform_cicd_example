2d6e7430cf75a6b7a2ee62695bbcc957b0f6797a
theodore-q <theo.q.er@gmail.com>
2023-06-17 11:39:39 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "template_file" "user_data" {
+  template = file("${path.module}/user_data.sh")
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c94855ba95c574c8" # update with your preferred AMI ID
   instance_type = "t2.micro"
+  user_data     = data.template_file.user_data.rendered
 
   tags = {
     Name = "terraform-example"
